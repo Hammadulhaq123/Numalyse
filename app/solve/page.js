9c6779d9d577ee9def4fe39938dc06700cc3c61d
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-07 03:35:00 +0500

    Created  a route that is going to contain list of all the funcitonalities that our website is going to perform

---
+++ app/solve/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            List of all the functions that we execute
+        </div>
+    )
+}
+
+export default page
