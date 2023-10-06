3d142e342af9fd102a43cd8bb1dab250f907256b
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-07 03:32:07 +0500

    Developed this component to show a title and short description about the program/funnction that the user is going to execute on the page

---
+++ components/Describer.js
@@ -0,0 +1,15 @@
+
+const Describer = ({ title, description }) => {
+    return (
+        <div className="w-full h-auto bg-[#04aa6d] flex flex-col items-center lg:items-start justify-start rounded-[16px] p-8">
+            <h1 className="text-5xl lg:text-7xl mb-2 w-full lg:w-[80%] font-bold text-white  ">
+                {title}
+            </h1>
+            <p className="text-md w-full lg:w-[70%] font-light text-gray-50 ">
+                {description}
+            </p>
+        </div>
+    )
+}
+
+export default Describer
