8ca9109e544ff1230a344c8e0b7fb870807edc2a
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-07 22:21:01 +0500

    Updated the button to a link to the list of all the functionalities

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,17 @@
 import { BsArrowUpRight } from 'react-icons/bs'
+import Link from 'next/link'
+
+
 const Button = () => {
     return (
-        <button className="w-auto bg-[#2e2d2d] text-[#04aa6d] text-sm font-bold rounded-full h-12 py-2 px-4 flex items-center gap-2 justify-between">
+        <Link href="/solve" className="w-auto bg-[#2e2d2d] text-[#04aa6d] text-sm font-bold rounded-full h-12 py-2 px-4 flex items-center gap-2 justify-between">
             Try for Free
             <span className='w-8 h-8 bg-[#04aa6d] rounded-full flex justify-center items-center'>
                 <BsArrowUpRight className='text-white' />
             </span>
 
 
-        </button>
+        </Link>
     )
 }
 
