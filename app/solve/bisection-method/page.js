68e7e7d5adf655cb575b02dfd2224bf8b04f6b84
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-08 19:31:38 +0500

    Updated few functionalities added new graphical representaion of the function and it's root

--- app/solve/bisection-method/page.js
+++ app/solve/bisection-method/page.js
@@ -3,7 +3,16 @@ import Navbar from '../../../components/Navbar'
 import { BsFacebook, BsInstagram } from "react-icons/bs"
 import { AiOutlineLinkedin } from "react-icons/ai"
 import BisectionInputs from '../../../RequiredInputs/BisectionInputs'
+import { Cedarville_Cursive } from "next/font/google"
 
+
+
+
+
+const supreme = Cedarville_Cursive({
+    weight: '400',
+    subsets: ['latin']
+})
 const page = () => {
     return (
         <>
@@ -17,8 +26,8 @@ const page = () => {
                 </div>
 
                 {/* SideBar that is going to contain the ads and extras */}
-                <div className='w-[30%] h-[97vh] hidden lg:flex flex-col bg-gray-200 items-center justify-start p-4 text-[#04aa6d] rounded-[20px]'>
-                    <div className='flex gap-2 items-center justify-center flex-col text-3xl font-bold text-[#04aa6d]'>
+                <div className='w-[30%] h-auto hidden lg:flex flex-col bg-gray-200 items-center justify-start py-8 px-4 text-[#04aa6d] rounded-[20px]'>
+                    {/* <div className='flex gap-2 items-center justify-center flex-col text-3xl font-bold text-[#04aa6d]'>
                         Share Analysis!
                         <div className='flex gap-2 text-xl'>
                             <button className='w-8 h-8 bg-[#04aa6d] text-[#fff] rounded-full flex items-center justify-center'>
@@ -31,6 +40,12 @@ const page = () => {
                                 <AiOutlineLinkedin />
                             </button>
                         </div>
+                    </div> */}
+                    <div id='plot' className={` w-full text-center h-auto flex gap-2 items-center justify-center flex-col text-5xl font-bold text-[#04aa6d]`}>
+                        <span className={`${supreme.className}`}>
+                            Graphical Representation
+
+                        </span>
                     </div>
                 </div>
             </div>
