7b427e7e15f5a67476a0658149f97cf599157136
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-11 23:11:59 +0500

    Updated the page.js

--- app/page.js
+++ app/page.js
@@ -19,12 +19,12 @@ export default function Home() {
       <div className="flex gap-4 w-full h-auto lg:h-[39%] flex-wrap lg:flex-nowrap">
         <div className="w-full h-[19rem] bg-[#04aa6d] lg:w-[29%] lg:h-full rounded-[20px] flex flex-col justify-center gap-2 items-center p-4">
           <h1 className={` w-[90%] text-[#fff] text-center text-5xl font-extrabold `}>
-            Other Method
+            Secant Method
           </h1>
           <p className="w-8/12 text-center text-gray-200 text-lg font-normal">
-            Solve the Bisction Method efficiently using <span className={`${supreme.className} text-[#2e2e2d]`}>Numalyse.</span>
+            Solve the Secant Method efficiently using <span className={`${supreme.className} text-[#2e2e2d]`}>Numalyse.</span>
           </p>
-          <Link href="/solve/bisection-method" className="w-auto bg-gray-100 text-[#04aa6d] text-sm font-bold rounded-full h-12 py-2 px-4 flex items-center gap-2 justify-between">
+          <Link href="/solve/secant-method" className="w-auto bg-gray-100 text-[#04aa6d] text-sm font-bold rounded-full h-12 py-2 px-4 flex items-center gap-2 justify-between">
             Calculate
             <span className='w-8 h-8 bg-[#04aa6d] rounded-full flex justify-center items-center'>
               <BsArrowUpRight className='text-white' />
