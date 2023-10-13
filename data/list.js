ed82ca0b75754b7ab0ca13419a97aef71de0c0f0
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-13 20:01:17 +0500

    Created a directory that has our list.js file that has all the categories and names of the methods that we are providing to the user

---
+++ data/list.js
@@ -0,0 +1,38 @@
+export const methods = [
+    {
+        "methodCategory": "Mathematics", "methodList": [
+            {
+                "methodName": "Quadratic Equation Solver",
+                "methodLink": "/solve/quadratic-equation-solver",
+                "style": "greenBorderBox"
+
+            }
+        ]
+    },
+    {
+        "methodCategory": "Numerical Analysis", "methodList": [
+            {
+                "methodName": "Bisection Method",
+                "methodLink": "/solve/bisection-method",
+                "style": "grayBackgroundBox"
+            },
+            {
+                "methodName": "Newton Method",
+                "methodLink": "/solve/newton-method",
+                "style": "greenBorderBox"
+
+            },
+            {
+                "methodName": "Secant Method",
+                "methodLink": "/solve/secant-method",
+                "style": "greenBackgroundBox"
+
+            },
+            // {
+            //     "methodName": "Fixed Point Method",
+            //     "methodLink": "/solve/fixed-point-method",
+            //     "style": "grayBorderBox"
+            // }
+        ]
+    },
+]
