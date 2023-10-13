35167503555f21339f8147a4fc4a5620f5120c9c
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-14 01:10:28 +0500

    Layout fixed for sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -26,7 +26,7 @@ const links = [
 ]
 const Sidebar = () => {
     return (
-        <div id="sidebar-main" className='fixed top-0 overflow-y-auto transition-all duration-200 -translate-x-60 left-0 w-60 p-4 h-screen flex flex-col justify-start items-center gap-2 border-r-4 border-[#04aa6d] bg-white rounded-r-xl z-50'>
+        <div id="sidebar-main" className='fixed top-0 overflow-y-auto transition-all duration-200 -translate-x-60 left-0 w-60 p-4 h-screen flex flex-col justify-start items-center gap-2 shadow-sm shadow-[#2e2e2e] bg-white rounded-r-xl z-50'>
             <button onClick={sidebarClose} className="absolute top-2 right-2 flex items-center justify-center text-white w-8 h-8 bg-[#04aa6d] rounded-lg">
                 <IoClose />
             </button>
