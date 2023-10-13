aac2774217e289cf123c120d9a3fe4861cb397a2
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-13 21:47:47 +0500

    Styles and minor func updates

--- app/solve/components/AddOnSidebar.js
+++ app/solve/components/AddOnSidebar.js
@@ -1,8 +1,8 @@
-import { Cedarville_Cursive } from "next/font/google"
+import { Inconsolata } from "next/font/google"
 
 
-const supreme = Cedarville_Cursive({
-    weight: '400',
+const supreme = Inconsolata({
+    weight: '800',
     subsets: ['latin']
 })
 
@@ -11,8 +11,8 @@ const AddOnSidebar = () => {
     return (
         <>
             {/* SideBar that is going to contain the ads and extras */}
-            <div className='w-[30%] h-auto hidden lg:flex flex-col bg-[#2e2e2e] items-center justify-start py-8 px-4 text-[#fff] rounded-[20px]'>
-                <div id='plot' className={` w-full text-center h-auto flex gap-2 items-center justify-center flex-col text-5xl font-bold text-[#fff]`}>
+            <div className='w-[30%] h-auto hidden lg:flex flex-col bg-gray-200 border-4 border-[#2e2e2e] border-dashed items-center justify-start py-8 px-4 text-[#2e2e2e] rounded-[20px]'>
+                <div id='plot' className={` w-full text-center h-auto flex gap-2 items-center justify-center flex-col text-5xl font-bold text-[#2e2e2e]`}>
                     <span className={`${supreme.className}`}>
                         Graphical Representation
 
