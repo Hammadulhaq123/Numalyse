6bab73d14e5d9c0f51fd6a72dc11b4307953a355
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-13 22:24:25 +0500

    background and little func updated

--- app/solve/components/AddOnSidebar.js
+++ app/solve/components/AddOnSidebar.js
@@ -11,11 +11,10 @@ const AddOnSidebar = () => {
     return (
         <>
             {/* SideBar that is going to contain the ads and extras */}
-            <div className='w-[30%] h-auto hidden lg:flex flex-col bg-gray-200 border-4 border-[#2e2e2e] border-dashed items-center justify-start py-8 px-4 text-[#2e2e2e] rounded-[20px]'>
-                <div id='plot' className={` w-full text-center h-auto flex gap-2 items-center justify-center flex-col text-5xl font-bold text-[#2e2e2e]`}>
+            <div className='w-[30%] h-auto hidden lg:flex flex-col bg-gray-100 border-4 border-[#2e2e2e] border-dashed items-center justify-start py-8 px-4 text-[#2e2e2e] rounded-[20px]'>
+                <div id='plot' className={`hidden w-full text-center h-auto gap-2 items-center justify-center flex-col text-5xl font-bold text-[#2e2e2e]`}>
                     <span className={`${supreme.className}`}>
                         Graphical Representation
-
                     </span>
                 </div>
             </div>
