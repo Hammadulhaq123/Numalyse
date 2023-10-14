ac4c32886d5717db0e0b112c055e1e121c0d6b35
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-14 15:27:58 +0500

    Nothing updated

--- app/solve/components/GrayBox.js
+++ app/solve/components/GrayBox.js
@@ -5,7 +5,6 @@ const GrayBox = ({ name, link }) => {
     return (
         <Link href={link} className='w-full md:w-[21.1rem] lg:w-[19.1rem] xl:w-[20.4rem] text-xl font-bold p-4 flex justify-center items-center h-auto bg-[#2e2e2e] text-[#fff] rounded-[16px]'>
             {name}
-
         </Link>
     )
 }
