da073a333ba828670f9d7be7c8045552a74a1d44
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-14 15:25:46 +0500

    rendered the sidebar links using the global list of method from the data dir

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,18 @@
 "use client"
 import Link from "next/link"
-import { Cedarville_Cursive } from "next/font/google"
+import { Cedarville_Cursive, Inconsolata } from "next/font/google"
 import { IoClose } from "react-icons/io5";
 import SidebarLinks from "./SidebarLinks";
+import { methodsList } from "../data/list";
+
+
 const supreme = Cedarville_Cursive({
     weight: '400',
+    subsets: ['latin']
+})
+
+const inter = Inconsolata({
+    weight: '800',
     subsets: ['latin']
 })
 
@@ -14,16 +22,6 @@ const sidebarClose = () => {
     sidebar.classList.toggle("translate-x-0");
 }
 
-const links = [
-    {
-        link: "/solve/method/bisection-method",
-        text: "Bisection Method",
-    },
-    {
-        link: "/solve/method/newton-method",
-        text: "Newton Method",
-    },
-]
 const Sidebar = () => {
     return (
         <div id="sidebar-main" className='fixed top-0 overflow-y-auto transition-all duration-200 -translate-x-60 left-0 w-60 p-4 h-screen flex flex-col justify-start items-center gap-2 shadow-sm shadow-[#2e2e2e] bg-white rounded-r-xl z-50'>
@@ -34,10 +32,25 @@ const Sidebar = () => {
                 Numalyse
             </Link>
             {
-                links.map((link, key) => (
-                    <SidebarLinks key={key} link={link.link} text={link.text} />
+                methodsList?.map((method, key) => {
+                    return (
 
-                ))
+                        <>
+                            <h1 className={`${inter.className} my-1 text-lg `} key={key}>{method?.methodCategory}</h1>
+
+                            {
+                                method?.methodList?.map((link, key) => {
+                                    return (
+                                        <SidebarLinks key={key} link={link.methodLink} text={link.methodName} />
+                                    )
+                                }
+                                )
+                            }
+                        </>
+                    )
+                }
+
+                )
             }
         </div>
     )
