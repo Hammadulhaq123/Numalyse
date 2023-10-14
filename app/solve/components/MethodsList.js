b1e925965bf27359f1214d747afe0a2043139bbb
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-14 14:11:05 +0500

    Search filter functionality and sidbar alphabet scroller implemented. Search functionality is not working as I expect it to work will fix it later

--- app/solve/components/MethodsList.js
+++ app/solve/components/MethodsList.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from 'react'
+import { useState, useDeferredValue, useEffect } from 'react'
 import { methodsList } from '../../../data/list'
 import { Inconsolata } from "next/font/google"
 import { FiSearch } from "react-icons/fi"
@@ -18,94 +18,161 @@ const supreme = Inconsolata({
 })
 
 const MethodsList = () => {
-    const [methods, setMethods] = useState(methodsList)
+    const [methods, setMethods] = useState(methodsList);
+    const [filtered, setFiltered] = useState(methods);
+    const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    const handleClick = (id) => {
+        const elem = document.getElementById(id);
+        elem && elem.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
+
     const [value, setValue] = useState("");
+    const searchValue = useDeferredValue(value);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        if (searchValue == "") return setFiltered(methods);
+
+        setTimeout(() => {
+            setFiltered(methods.filter((method) => {
+                if (method.methodCategory.toLowerCase().includes(searchValue.toLowerCase())) {
+                    return (method.methodCategory.toLowerCase().includes(searchValue.toLowerCase()))
+                }
+                else {
+                    return (null)
+                }
+            }))
+        }, 500)
+    }
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setMethods(methods.filter((value) => {
-            return (value)
+        if (searchValue == "") return setFiltered(methods);
+        setFiltered(methods.filter((method) => {
+            if (method.methodCategory.toLowerCase().includes(searchValue.toLowerCase())) {
+                return (method.methodCategory.toLowerCase().includes(searchValue.toLowerCase()))
+            }
+            else {
+                return (null)
+            }
         }))
-        console.log(methods);
+
     }
+
+
     return (
         <div className='p-4 h-screen overflow-x-hidden overflow-y-auto bg-white'>
             <Navbar />
             <form onSubmit={(e) => handleSubmit(e)} className='w-full h-auto flex gap-2'>
 
-                <input type='text' placeholder='Search Category or Method' className='bg-white outline-none border-4 border-[#04aa6c] px-4 text-[#04aa6c] font-semibold text-lg w-11/12 h-12 rounded-full' value={value} onChange={(e) => setValue(e.target.value)} name='search' />
+                <input type='text' placeholder='Search Category' className='bg-white outline-none border-4 border-[#04aa6c] px-4 text-[#04aa6c] font-semibold text-lg w-11/12 h-12 rounded-full' value={searchValue} onChange={(e) => handleChange(e)} name='search' autoComplete='off' />
                 <button type='submit' className='w-1/12 h-12 rounded-full bg-[#04aa6c] text-white  flex justify-center items-center'>
                     <FiSearch className='font-bold text-xl' />
                 </button>
             </form>
 
-            {
-                methods
-                    ?.sort(function (a, b) {
-                        var catA = a.methodCategory.toUpperCase(); // ignore upper and lowercase
-                        var catB = b.methodCategory.toUpperCase(); // ignore upper and lowercase
-                        if (catA < catB) {
-                            return -1;
-                        }
-                        if (catA > catB) {
-                            return 1;
-                        }
-                        // names must be equal
-                        return 0;
-                    })
-                    ?.map((method, key) => {
-                        return (
-                            <div key={key}>
-                                <div className='w-full h-auto flex p-4 items-center justify-start'>
+            <div id='list' className='w-full h-auto flex gap-1'>
+                <div className='w-[96%] h-auto'>
+                    {
 
-                                    <h1 className={` ${supreme.className} text-3xl font-bold text-[#04aa6c]`}>
-                                        {method?.methodCategory}
-                                    </h1>
-                                </div>
+                        filtered
+                            ?.sort(function (a, b) {
+                                var catA = a.methodCategory.toUpperCase(); // ignore upper and lowercase
+                                var catB = b.methodCategory.toUpperCase(); // ignore upper and lowercase
+                                if (catA < catB) {
+                                    return -1;
+                                }
+                                if (catA > catB) {
+                                    return 1;
+                                }
+                                // cateogry must be equal
+                                return 0;
+                            })
+                            // ?.filter((method) => {
+                            //     return (
+                            //         searchValue == "" ? method : method.methodCategory.toLowerCase().includes(searchValue.toLowerCase())
+                            //     )
+                            // })
+                            ?.map((method, key) => {
+                                return (
+                                    <div key={key}>
 
-                                <div className='flex w-full h-auto rounded-[20px] bg-gray-100 justify-start flex-wrap gap-1 p-2'>
-                                    {
+                                        <div id={method.id} className='w-full h-auto flex p-4 items-center justify-start'>
 
-                                        method
-                                            ?.methodList
-                                            ?.sort(function (a, b) {
-                                                var nameA = a.methodName.toUpperCase(); // ignore upper and lowercase
-                                                var nameB = b.methodName.toUpperCase(); // ignore upper and lowercase
-                                                if (nameA < nameB) {
-                                                    return -1;
+                                            <h1 className={` ${supreme.className} text-3xl font-bold text-[#04aa6c]`}>
+                                                {method?.methodCategory}
+                                            </h1>
+                                        </div>
+                                        {
+                                            <div className='flex w-full h-auto rounded-[20px] bg-gray-100 justify-start flex-wrap gap-1 p-2'>
+                                                {
+                                                    method
+                                                        ?.methodList
+                                                        ?.sort(function (a, b) {
+                                                            var nameA = a.methodName.toUpperCase(); // ignore upper and lowercase
+                                                            var nameB = b.methodName.toUpperCase(); // ignore upper and lowercase
+                                                            if (nameA < nameB) {
+                                                                return -1;
+                                                            }
+                                                            if (nameA > nameB) {
+                                                                return 1;
+                                                            }
+                                                            // names must be equal
+                                                            return 0;
+                                                        })
+                                                        // ?.filter((item) => {
+                                                        //     return (
+                                                        //         searchValue == "" ? item : item.methodName.toLowerCase().includes(searchValue.toLowerCase())
+                                                        //     )
+                                                        // })
+                                                        ?.map((item, key) => (
+                                                            // Checking Items style and rendering the component of the given style
+                                                            (item?.style == "greenBorderBox") ?
+
+                                                                <GreenBorder key={key} className='text-green-500' name={item?.methodName} link={item?.methodLink} />
+
+                                                                : (item?.style == "greenBackgroundBox") ?
+                                                                    <GreenBox key={key} name={item?.methodName} link={item?.methodLink} />
+
+                                                                    : (item?.style == "grayBackgroundBox") ?
+                                                                        <GrayBox key={key} name={item?.methodName} link={item?.methodLink} />
+
+                                                                        : (item?.style == "grayBorderBox") &&
+                                                                        <GrayBorder key={key} name={item?.methodName} link={item?.methodLink} />
+
+
+
+                                                        ))
                                                 }
-                                                if (nameA > nameB) {
-                                                    return 1;
-                                                }
-                                                // names must be equal
-                                                return 0;
-                                            })
-                                            ?.map((item, key) => (
-                                                // Checking Items style and rendering the component of the given style
-                                                (item?.style == "greenBorderBox") ?
+                                            </div>
+                                        }
 
-                                                    <GreenBorder key={key} className='text-green-500' name={item?.methodName} link={item?.methodLink} />
+                                    </div>
+                                )
 
-                                                    : (item?.style == "greenBackgroundBox") ?
-                                                        <GreenBox key={key} name={item?.methodName} link={item?.methodLink} />
+                            })
 
-                                                        : (item?.style == "grayBackgroundBox") ?
-                                                            <GrayBox key={key} name={item?.methodName} link={item?.methodLink} />
+                    }
+                    {
+                        (filtered.length == 0) && <h1>Nothing here</h1>
+                    }
+                </div>
+                <div className='flex flex-col items-center  justify-start w-[4%] h-auto text-sm font-bold  text-[#2e2e2e] gap-[8px] py-12'>
+                    {
+                        alphabets.map((item, key) => (
+                            <button key={key} className='outline-none border-none' onClick={() => handleClick(item)}>
+                                {item}
+                            </button>
+                        ))
+                    }
+                </div>
+            </div>
 
-                                                            : (item?.style == "grayBorderBox") &&
-                                                            <GrayBorder key={key} name={item?.methodName} link={item?.methodLink} />
-
-
-
-                                            ))
-                                    }
-                                </div>
-                            </div>
-
-                        )
-
-                    })
-            }
         </div>
     )
 }
