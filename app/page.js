5878cded2afd4900cad4d7bf1d9f2b29473d5f39
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-19 14:13:01 +0500

    Update: Rendered the Steffen comp/card in the page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
 import Hero from "@/components/Hero";
 import Bisection from "@/components/Bisection";
 import Newton from "../components/Newton";
-import { Cedarville_Cursive } from "next/font/google"
-import Link from "next/link"
-import { BsArrowUpRight } from "react-icons/bs"
 import Secant from "../components/Secant";
-const supreme = Cedarville_Cursive({
-  weight: '400',
-  subsets: ['latin']
-})
+import Stefen from "../components/Stefen";
+
 export default function Home() {
   return (
     <div className="flex flex-col bg-white w-full h-screen gap-4 p-4 items-start justify-start">
@@ -19,22 +14,7 @@ export default function Home() {
 
       <div className="flex gap-4 w-full h-auto lg:h-[39%] flex-wrap lg:flex-nowrap">
         <Secant />
-        <div className="w-full h-[19rem] bg-gray-100 border-4 border-[#04aa6d] border-dashed lg:w-[30.5%] lg:h-full rounded-[20px] flex flex-col justify-center gap-4 items-center p-4">
-          <h1 className={` w-[90%] text-[#1c1c1c] text-center text-5xl font-extrabold `}>
-            Another Method
-          </h1>
-          <p className="w-8/12 text-center text-lg font-normal">
-            Solve the Bisction Method efficiently using <span className={supreme.className}>Numalyse.</span>
-          </p>
-          <Link href="/solve/method/bisection-method" className="w-auto bg-[#2e2d2d] text-[#04aa6d] text-sm font-bold rounded-full h-12 py-2 px-4 flex items-center gap-2 justify-between">
-            Calculate
-            <span className='w-8 h-8 bg-[#04aa6d] rounded-full flex justify-center items-center'>
-              <BsArrowUpRight className='text-white' />
-            </span>
-
-
-          </Link>
-        </div>
+        <Stefen />
         <Newton />
       </div>
 
