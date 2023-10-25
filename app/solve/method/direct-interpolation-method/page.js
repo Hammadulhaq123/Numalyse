52a7622043d239adfd61c5f3e9ce6ee66cefd181
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-25 21:26:44 +0500

    styling updated

--- app/solve/method/direct-interpolation-method/page.js
+++ app/solve/method/direct-interpolation-method/page.js
@@ -9,7 +9,7 @@ const page = () => {
             {/* Main Functionalities */}
             <div className='w-full lg:w-[70%] bg-gray-100 p-4 gap-2 rounded-[20px] flex flex-col items-center justify-start'>
                 <Navbar />
-                <Describer title={"Direct Interpolation Method"} description={"The direct method (also called the Vandermonde polynomial method) of interpolation is based on the following premise. Given  n+1 data points, fit a polynomial of order  n as given below y=a0+a1x+……………+anx^n."} />
+                <Describer title={"Direct Interpolation"} description={"The direct method (also called the Vandermonde polynomial method) of interpolation is based on the following premise. Given  n+1 data points, fit a polynomial of order  n as given below y=a0+a1x+……………+anx^n."} />
                 <DirectInterpolationInput />
             </div>
 
--- components/Describer.js
+++ components/Describer.js
@@ -1,7 +1,7 @@
 
 const Describer = ({ title, description }) => {
     return (
-        <div className="w-full h-auto bg-[#04aa6d] flex flex-col items-center lg:items-start justify-start rounded-[16px] p-8">
+        <div className="w-full h-auto bg-[#0e0e10] flex flex-col items-center lg:items-start justify-start rounded-[16px] p-8">
             <h1 className="text-5xl lg:text-7xl mb-2 w-full lg:w-[80%] font-bold text-white  ">
                 {title}
             </h1>
